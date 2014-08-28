d1a8c8806087f90ec9e8b49fc2e55918d6c6185d
BaBeuloula <contact@babeuloula.fr>
2014-08-28 07:56:12 +0200

    Réécriture total de l'application

--- app/js/node.js
+++
@@ -1,6 +0,0 @@
-window.ondragover = function(e) {e.preventDefault(); return false; }
-window.ondrop = function(e) {e.preventDefault(); return false; }
-
-var execFile = require('child_process').execFile, child;
-var path = require('path');
-var gui = require('nw.gui');
